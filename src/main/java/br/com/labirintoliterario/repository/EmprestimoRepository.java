4870e29e70724addf41c6041e3051a64502803fb
Felipe Daniel <felipedaniels622@gmail.com>
2026-05-18 20:31:10 -0300

    Regra de negocio emprestimo

--- src/main/java/br/com/labirintoliterario/repository/EmprestimoRepository.java
+++ src/main/java/br/com/labirintoliterario/repository/EmprestimoRepository.java
@@ -1,12 +1,10 @@
 package br.com.labirintoliterario.repository;
 
 import br.com.labirintoliterario.entity.Emprestimo;
-import br.com.labirintoliterario.maper.StatusEmprestimo;
+import br.com.labirintoliterario.mapper.StatusEmprestimo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.w3c.dom.stylesheets.LinkStyle;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface EmprestimoRepository extends JpaRepository<Emprestimo, Long> {
 
